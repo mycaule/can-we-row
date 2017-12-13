61386e3054a9ade9290228681cc88c40aeb4033c
Michel Hua <michel.hua@mfglabs.com>
2017-12-13 16:59:05 +0100

    City selector

--- public/app.js
+++ public/app.js
@@ -1,4 +1,53 @@
-/* global moment, v */
+/* global moment, Mavo */
+
+const findStation = city => {
+  switch (city) {
+    case 'amiens': return 'E640091001'
+    case 'besancon': return 'U251542001'
+    case 'bordeaux': return 'O919001001'
+    case 'caen': return 'I362101001'
+    case 'chalons': return 'H520101003'
+    case 'clermont': return 'K322201001'
+    case 'dijon': return 'U132401001'
+    case 'lille': return 'E381126501'
+    case 'limoges': return 'L040061002'
+    case 'lyon': return 'V163002002'
+    case 'marseille': return 'Y442404001'
+    case 'metz': return 'A743061001'
+    case 'montpellier': return 'Y321001001'
+    case 'nantes': return 'M530001010'
+    case 'orleans': return 'K435001010'
+    case 'paris': return 'F700000103'
+    case 'poitiers': return 'L250161001'
+    case 'rennes': return 'J709063002'
+    case 'rouen': return 'H438021010'
+    case 'strasbourg': return 'A061005051'
+    case 'toulouse': return 'O200001001'
+    default: return undefined
+  }
+}
+
+const linkTo = city => {
+  const station = findStation(city)
+
+  if (station) {
+    return `/?city=${city}&station=${station}`
+  }
+
+  return '/'
+}
+
+Mavo.Functions.onchange = newCity => {
+  const oldUrl = new URL(window.location.href)
+  const oldParams = oldUrl.searchParams
+  const oldCity = oldParams.get('city')
+
+  if (newCity === oldCity) {
+    console.log('staying here!')
+  } else {
+    window.location.href = linkTo(newCity)
+  }
+}
 
 const $ = s => document.querySelector(s)
 
@@ -6,6 +55,15 @@ const url = new URL(window.location.href)
 const params = url.searchParams
 const city = params.get('city')
 const station = params.get('station')
+
+const selectItemByValue = (elt, value) => {
+  for (let i = 0; i < elt.options.length; i++) {
+    if (elt.options[i].value === value) {
+      elt.selectedIndex = i
+      break
+    }
+  }
+}
 
 const reloadMetrics = (city, station) => () =>
     Promise.all([
@@ -21,6 +79,8 @@ if (city === null || station === null) {
   window.location.href = '/?city=paris&station=F700000103'
 } else {
   $('.refresh-data').onclick = reloadMetrics(city, station)
+  selectItemByValue($('#cities'), city)
+  $('#cities').value = city
 
   fetch(`/latest/temperatures?cities=${city}`)
     .then(response => {
@@ -30,7 +90,6 @@ if (city === null || station === null) {
             reloadMetrics(city, station)()
           } else {
             const curr = data.find(elt => elt.city === city)
-            $('.title').textContent += ` à ${v.titleCase(curr.city)}`
             $('.temperature').textContent = `${curr.meas.toFixed(1)} °C`
             $('.temperature-time').textContent = moment.unix(curr.time / 1000).lang('fr').fromNow()
 
