00923baa16149efdd25bf0d9d8836390e8fc33ef
mycaule <michel.hua@gmail.com>
2018-03-22 17:43:13 +0100

    update dependencies

--- metrics/vigicrues.test.js
+++ metrics/vigicrues.test.js
@@ -6,12 +6,12 @@ test('stations', async t => {
   const stationsH = await vigicrues.stations()
   t.true(stationsH.Observations.NbElements > 1700)
   t.true(stationsH.PasObservations.NbElements < 50)
-  t.is(stationsH.VersionFlux, 'Beta 0.4a')
+  t.is(stationsH.VersionFlux, 'Beta 0.4b')
 
   const stationsQ = await vigicrues.stations('Q')
   t.true(stationsQ.Observations.NbElements > 1300)
   t.true(stationsQ.PasObservations.NbElements < 500)
-  t.is(stationsQ.VersionFlux, 'Beta 0.4a')
+  t.is(stationsQ.VersionFlux, 'Beta 0.4b')
 })
 
 test('observations', async t => {
@@ -20,27 +20,27 @@ test('observations', async t => {
   const obsQIso = await vigicrues.observations('F700000103', 'Q', 'iso')
   const obsQIsoSimple = await vigicrues.observations('F700000103', 'Q', 'iso', 'simple')
 
-  t.is(obsH.VersionFlux, 'Beta 0.4a')
-  t.is(obsQ.VersionFlux, 'Beta 0.4a')
-  t.is(obsQIso.VersionFlux, 'Beta 0.4a')
-  t.is(obsQIsoSimple.VersionFlux, 'Beta 0.4a')
+  t.is(obsH.VersionFlux, 'Beta 0.4b')
+  t.is(obsQ.VersionFlux, 'Beta 0.4b')
+  t.is(obsQIso.VersionFlux, 'Beta 0.4b')
+  t.is(obsQIsoSimple.VersionFlux, 'Beta 0.4b')
 })
 
 test('informations', async t => {
   const informations = await vigicrues.informations('F700000102')
-  t.is(informations.VersionFlux, 'Beta 0.3g')
+  t.is(informations.VersionFlux, 'Beta 0.3i')
 })
 
 test('previsions', async t => {
   const infos1 = await vigicrues.informations('F700000103')
   const infos2 = await vigicrues.informations('M530001010')
-  t.is(infos1.VersionFlux, 'Beta 0.3g')
-  t.is(infos2.VersionFlux, 'Beta 0.3g')
+  t.is(infos1.VersionFlux, 'Beta 0.3i')
+  t.is(infos2.VersionFlux, 'Beta 0.3i')
 
   const prevsH = await vigicrues.previsions('M530001010', 'H')
   const prevsQ = await vigicrues.previsions('M530001010', 'Q')
-  t.is(prevsH.VersionFlux, 'Beta 0.4e')
-  t.is(prevsQ.VersionFlux, 'Beta 0.4e')
+  t.is(prevsH.VersionFlux, 'Beta 0.4f')
+  t.is(prevsQ.VersionFlux, 'Beta 0.4f')
 })
 
 test('bulletin', async t => {
